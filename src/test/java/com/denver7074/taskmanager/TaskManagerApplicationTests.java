1e30f795d7e8351e303944598c2db1298493cbc5
dstolbinskij <denver7074@yandex.ru>
2023-12-06 16:34:22 +0400

    start

---
+++ src/test/java/com/denver7074/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denver7074.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
